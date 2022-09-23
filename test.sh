537393b7845bc58e639b1565f0c8f3ac71f76203
argenspin <86794565+argenspin@users.noreply.github.com>
2022-09-23 23:35:09 +0530

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,17 +1,25 @@
 sudo apt update
-#sudo apt upgrade
-sudo apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 libjpeg-dev gdebi -y
+sudo apt install python3-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev \
+    libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev libfreetype6-dev \
+    liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev python3-pip gdebi -y
 
 sudo apt-get install nodejs npm -y
 sudo npm install -g rtlcss
+
+wget -O libssl.deb http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
+sudo gdebi --n libssl.deb
 
 wget -O wkhtmltox.deb https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb
 sudo gdebi --n wkhtmltox.deb
 sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
 sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
 
-sudo apt install postgresql postgresql-client
+sudo apt install postgresql postgresql-client -y
 sudo -u postgres createuser -s $USER
 createdb $USER
 
 pip3 install setuptools wheel
+pip3 install -r requirements.txt
+sudo rm /usr/lib/python3/dist-packages/_cffi_backend.cpython-310-x86_64-linux-gnu.so
+pip3 install -r requirements.txt
+sed -i "s/ubuntu/$USER/g" odoo.conf
